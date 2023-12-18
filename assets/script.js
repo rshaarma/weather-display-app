1146b619fbf53b01224d7c307df106556c24c01f
rshaarma <rohittsharrma@outlook.com>
2023-12-18 22:16:55 +0000

    added fetch method to get data through coordinates

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,11 @@
-let button = document.getElementById("search-button");
-let cityInput = document.getElementById("search-input");
-let historySearches = document.getElementById("history-searches");
-let weatherToday = document.getElementById("weather-today");
-let clearHistoryBtn = document.getElementById("clear-history");
+const button = document.getElementById("search-button");
+const cityInput = document.getElementById("search-input");
+const historySearches = document.getElementById("history-searches");
+const weatherToday = document.getElementById("weather-today");
+const weatherCards = document.querySelector(".weather-cards");
+const clearHistoryBtn = document.getElementById("clear-history");
 
-let API_KEY = "bf9df75eeb5dfc02a2e3f7d42c3b052c";
+const API_KEY = "bf9df75eeb5dfc02a2e3f7d42c3b052c";
 
 function getCityCoordinates() {
   const cityName = cityInput.value.trim();
@@ -17,10 +18,60 @@ function getCityCoordinates() {
     })
     .then(function (data) {
       if (!data.length) return alert(`No coordinates found for ${cityName}`);
-      //   console.log(data);
-      let { lat, lon, name } = data[0];
+      const { lat, lon, name } = data[0];
       getWeatherDetails(name, lat, lon);
-      //   console.log(lat, lon, name);
     });
 }
+
+function getWeatherDetails(cityName, latitude, longitude) {
+  const COORD_URL = `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${API_KEY}`;
+  console.log(cityName);
+  fetch(COORD_URL)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+      const forecastDays = [];
+      const fiveDays = data.list.filter(function (forcast) {
+        const date = new Date(forcast.dt_txt).getDate();
+        if (!forecastDays.includes(date)) {
+          return forecastDays.push(date);
+        }
+      });
+      console.log(fiveDays);
+      cityInput.value = "";
+      weatherToday.value = "";
+      weatherCards.value = "";
+      fiveDays.forEach(function (item, index) {
+        if (index === 0) {
+          weatherToday.insertAdjacentHTML(
+            "beforeend",
+            `
+            <div class="current-forecast">
+                <h2>${cityName}(${dayjs(item.dt_txt.split(" ")[0]).format(
+              "DD / MM / YYYY"
+            )})</h2>
+            <h6>Temperature:${(item.main.temp - 273.15).toFixed(2)} °C</h6>
+            <h6>Wind:${((item.wind.speed * 18) % 5).toFixed(1)} KMH</h6>
+            <h6>Humidity:${item.main.humidity}%</h6>
+            </div>
+            <div>
+                    <img src="https://openweathermap.org/img/wn/${
+                      item.weather[0].icon
+                    }@2x.png" >
+                    <h6>${item.weather[0].description}</h6>
+            </div>
+            `
+          );
+        } else {
+        }
+      });
+    });
+}
+
 button.addEventListener("click", getCityCoordinates);
+cityInput.addEventListener(
+  "keyup",
+  (e) => e.key === "Enter" && getCityCoordinates()
+);
