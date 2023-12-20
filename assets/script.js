9d81f9e817a70367c54c6952e9bda4ee56376480
rshaarma <rohittsharrma@outlook.com>
2023-12-20 03:28:40 +0000

    added styling for the buttons

--- assets/script.js
+++ assets/script.js
@@ -25,7 +25,6 @@ function getUserCurrentCoordinates() {
   });
 }
 function getCityCoordinates(cityName) {
-  //   const cityName = cityInput.value.trim();
   fetch(
     `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&appid=${API_KEY}`
   )
@@ -64,18 +63,18 @@ function getWeatherDetails(cityName, latitude, longitude) {
             "beforeend",
             `
             <div class="current-forecast">
-                <h2>${cityName}(${dayjs(item.dt_txt.split(" ")[0]).format(
+                <h1>${cityName}(${dayjs(item.dt_txt.split(" ")[0]).format(
               "DD / MM / YYYY"
-            )})</h2>
+            )})</h1>
             <h6>Temperature:${(item.main.temp - 273.15).toFixed(2)} °C</h6>
             <h6>Wind:${((item.wind.speed * 18) % 5).toFixed(1)} KMH</h6>
             <h6>Humidity:${item.main.humidity}%</h6>
             </div>
-            <div>
-                    <img src="https://openweathermap.org/img/wn/${
+            <div class="weather-day-div">
+                    <img class="weather-day-image" src="https://openweathermap.org/img/wn/${
                       item.weather[0].icon
                     }@2x.png" >
-                    <h6>${item.weather[0].description}</h6>
+                    <h5>${item.weather[0].description}</h5>
             </div>
             `
           );
@@ -83,17 +82,17 @@ function getWeatherDetails(cityName, latitude, longitude) {
           weatherCards.insertAdjacentHTML(
             "beforeend",
             `
-                <li class="card col-lg-2 pt-3">
+                <li class="card col-lg-2 pt-2">
                     <h6>${dayjs(item.dt_txt.split(" ")[0]).format(
                       "DD/MM/YYYY"
                     )}</h6>    
                    <img class="cards-image" src="https://openweathermap.org/img/wn/${
                      item.weather[0].icon
                    }@2x.png">
-                   <h6 class="card-details mb-3">Temp: ${(
+                   <h6 class="card-details mb-2">Temp: ${(
                      item.main.temp - 273.15
                    ).toFixed(2)}°C</h6>
-                   <h6 class="card-details mb-3">Wind: ${(
+                   <h6 class="card-details mb-2">Wind: ${(
                      (item.wind.speed * 18) %
                      5
                    ).toFixed(1)}KMH</h6>
@@ -174,4 +173,10 @@ cityInput.addEventListener("keyup", function (e) {
     getCityCoordinates(city);
     renderButtons();
   }
+});
+
+clearHistoryBtn.addEventListener("click", function (e) {
+  e.preventDefault();
+  historySearches.innerHTML = "";
+  localStorage.clear();
 });
